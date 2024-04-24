a29438b67761f041fb096bc71e16b7cc30a42bf9
John Axel Eriksson <john@insane.se>
2024-04-24 17:33:27 +0200

    Update workflow

--- src/bin/zwift.rs
+++ src/bin/zwift.rs
@@ -1,6 +1,5 @@
 use std::collections::BTreeMap;
 use std::path::Path;
-use std::{thread, time};
 use zellij_tile::prelude::*;
 
 #[derive(Default)]
@@ -15,16 +14,12 @@ impl ZellijPlugin for State {
     }
 
     fn pipe(&mut self, pipe_message: PipeMessage) -> bool {
-        if pipe_message.name == "zproject_selection" {
+        if pipe_message.name == "zwift_selection" {
             match pipe_message.payload {
                 Some(payload) => {
                     let cwd = Path::new(payload.trim());
                     let name = cwd.file_stem().unwrap().to_str().unwrap();
                     eprintln!("Selected project: {name}, cwd {payload}");
-                    close_focus();
-                    eprintln!("sleeping...");
-                    thread::sleep(time::Duration::from_secs(5));
-                    eprintln!("switching...");
                     switch_session_with_layout(
                         Some(name),
                         LayoutInfo::File("dev".into()),
