bc98bac31739951b92b8eb36521fa7ae7c1871ea
John Axel Eriksson <john@insane.se>
2025-01-21 10:52:42 +0100

    fix: fix for rust 1.81+

--- flake/packages.nix
+++ flake/packages.nix
@@ -6,13 +6,13 @@
   }: let
     rustWasi = pkgs.rust-bin.stable.latest.default.override {
       extensions = ["rust-src" "rust-std"];
-      targets = ["wasm32-wasi"];
+      targets = ["wasm32-wasip1"];
     };
     craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustWasi;
   in {
     packages.default = craneLib.buildPackage {
       src = craneLib.cleanCargoSource (craneLib.path ../.);
-      cargoExtraArgs = "--target wasm32-wasi";
+      cargoExtraArgs = "--target wasm32-wasip1";
       doCheck = false;
     };
   };
